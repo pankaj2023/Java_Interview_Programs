68bf65b82f311f0a3e82d1ee3683a6fd02dcd759
pankaj2023 <pankajjamdade023@gmail.com>
2025-02-09 19:06:10 +0530

    Reverse String Java Code 02/09/2025

---
+++ src/ReverseString.java
@@ -0,0 +1,19 @@
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String s = "Pankaj";
+		int n = s.length();
+		char[] arr = s.toCharArray();
+		String reverse="";
+		for(int i=n-1;i>=0;i--)
+		{
+			reverse = reverse + arr[i]; 
+			
+		}
+		System.out.print(reverse);
+	}
+
+}
